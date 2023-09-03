8c90f07eb5297576f14d8889ab08b49e1b26d59a
Rooholla-Khorrambakht <learn.kho@gmail.com>
2023-09-02 23:21:36 -0400

    highlevel interface for real robot is added

---
+++ B1Py/__init__.py
@@ -0,0 +1,17 @@
+import platform
+import sys
+import os
+
+architecture = platform.architecture()[0]
+machine = platform.machine()
+
+if architecture == '64bit':
+    if 'x86_64' in machine:
+        lib_path = os.path.join(os.path.dirname(__file__), 'unitree_legged_sdk/amd64')
+    elif 'aarch64' in machine:
+        lib_path = os.path.join(os.path.dirname(__file__), 'unitree_legged_sdk/arm64')
+    else:
+        raise EnvironmentError("Unsupported architecture")
+    sys.path.append(lib_path)
+else:
+    raise EnvironmentError("Unsupported architecture")
