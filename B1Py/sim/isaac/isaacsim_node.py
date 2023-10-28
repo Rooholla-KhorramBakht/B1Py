269078ef2e70b22118a282034d77f44e6f5702a6
Rooholla-Khorrambakht <learn.kho@gmail.com>
2023-10-28 16:28:54 -0400

    replicator interface is added

--- B1Py/sim/isaac/isaacsim_node.py
+++ B1Py/sim/isaac/isaacsim_node.py
@@ -10,10 +10,8 @@ from omni.isaac.core import World
 from omni.isaac.core.utils.nucleus import get_assets_root_path
 from omni.isaac.core.utils.prims import define_prim, get_prim_at_path
 from omni.isaac.sensor import RotatingLidarPhysX
-import omni.replicator.core as rep
-from pxr import UsdGeom
-from omni.replicator.core import Writer, AnnotatorRegistry
 from B1Py.sim.isaac.b1 import UnitreeB1
+from B1Py.sim.isaac.utils import AnnotatorManager
 import cv2
 
 sys.path.append("/home/vr-station/projects/lcm/build/python")
@@ -53,41 +51,36 @@ b1 = world.scene.add(
 
 # Add Lidar
 lidar = world.scene.add(
-    RotatingLidarPhysX(
-        prim_path="/World/B1/imu_link/lidar",
-        name="lidar",
-        translation=[0.16, 0.0, 0.14],
-        orientation=[0.0, 0.0, 0.0, 1.0],
-    )
+    RotatingLidarPhysX(prim_path="/World/B1/imu_link/lidar", 
+                       name="lidar", 
+                       translation=[0.16,0.,0.14],
+                       orientation=[0., 0., 0., 1.])
 )
 lidar.add_depth_data_to_frame()
 lidar.add_point_cloud_data_to_frame()
 lidar.set_rotation_frequency(0)
-lidar.set_resolution([0.4, 2])
+lidar.set_resolution([0.4,2])
 # lidar.enable_visualization()
 lidar.prim.GetAttribute("highLod").Set(True)
 lidar.prim.GetAttribute("highLod").Set(True)
 
 world.reset()
 b1.initialize()
+
 # Add cameras
 # Create cameras
-
-camera = world.stage.DefinePrim("/World/B1/imu_link/Camera1", "Camera")
-# UsdGeom.Xformable(camera).AddTranslateOp().Set((0., 10., 20.))
-# UsdGeom.Xformable(camera).AddRotateXYZOp().Set((-15., 0., 0.))
-
-# Create render products
-rp1 = rep.create.render_product(str(camera.GetPrimPath()), resolution=(640, 480))
-rgb_annotators = []
-for rp in [rp1]:
-    rgb = rep.AnnotatorRegistry.get_annotator("pointcloud")
-    rgb.attach([rp])
-    rgb_annotators.append(rgb)
-
+ann = AnnotatorManager(world)
+ann.registerCamera("/World/B1/imu_link", "camera_left", (0.485, 0.0258, 0.028), (-90., 180., 90.), resolution=(640, 480))   
+ann.registerCamera("/World/B1/imu_link", "camera_right", (0.485, 0.0258-0.05, 0.028), (-90., 180., 90.), resolution=(640, 480))   
+ann.registerAnnotator('rgb', 'camera_left')
+ann.registerAnnotator('rgb', 'camera_right')
+ann.registerAnnotator('distance_to_camera', 'camera_left')
+ann.setFocalLength('camera_left', 28)
+ann.setClippingRange('camera_left', 0.2, 1000000.0)
+ann.setFocalLength('camera_right', 28)
+ann.setClippingRange('camera_right', 0.2, 1000000.0)
 
 lcm_server = LCMBridgeServer(robot_name="b1")
-
 cmd_stamp = time.time()
 cmd_stamp_old = cmd_stamp
 
@@ -104,35 +97,40 @@ sim_manager = simulationManager(
     physics_dt=PHYSICS_DT,
     lcm_timeout=1e-4,
 )
+
 lidar_data_pipe = NumpyMemMapDataPipe(
     "lidar_data_pipe", force=True, dtype="float32", shape=(900, 16, 3)
 )
+left_rgb_data_pipe = NumpyMemMapDataPipe(
+    "left_rgb_data_pipe", force=True, dtype="uint8", shape=(480, 640, 4)
+)
+right_rgb_data_pipe = NumpyMemMapDataPipe(
+    "right_rgb_data_pipe", force=True, dtype="uint8", shape=(480, 640, 4)
+)
+depth_data_pipe = NumpyMemMapDataPipe(
+    "depth_data_pipe", force=True, dtype="float32", shape=(480, 640)
+)
 counter = 0
-
 while simulation_app.is_running():
     # sim_manager.step(counter*PHYSICS_DT)
     # Step the world with rendering 50 times per second
     sim_manager.step(counter * PHYSICS_DT)
-    pc = lidar.get_current_frame()["point_cloud"]
-    lidar_data_pipe.write(pc, match_length=True)
-
     if counter % 2 == 0:
         world.step(render=True)
-        # breakpoint()
         pc = lidar.get_current_frame()['point_cloud']
-        # print(pc.shape)
         lidar_data_pipe.write(pc, match_length=True)
-        # rep.orchestrator.step()
-        # Get annotator data after each replicator process step
-        for j, rgb_annot in enumerate(rgb_annotators):
-            # print(rgb_annot.get_data().shape)
-            # breakpoint()
-            img = rgb_annot.get_data()
-            # breakpoint()
-            print(img['data'].shape)
-            # if img.shape[0] !=0:
-                # cv2.imshow(f"rgb{j}", rgb_annot.get_data())
-        cv2.waitKey(1)
+
+        img_left = ann.getData('camera_left:rgb')
+        img_right = ann.getData('camera_right:rgb')
+        depth_left = ann.getData('camera_left:distance_to_camera')
+        if img_left.shape[0]!=0 and img_right.shape[0]!=0:
+            # cv2.imshow(f"rgb-left", img_left)
+            # cv2.imshow(f"rgb-right", img_right)
+            # cv2.imshow(f"depth", depth_left)
+            left_rgb_data_pipe.write(img_left, match_length=False)
+            right_rgb_data_pipe.write(img_right, match_length=False)
+            depth_data_pipe.write(depth_left, match_length=False)
+        # cv2.waitKey(1)
     else:
         world.step(render=False)
 
