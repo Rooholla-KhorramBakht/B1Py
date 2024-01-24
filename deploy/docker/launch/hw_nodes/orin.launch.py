dba4f32038a6d1a1eb85c184e0155abb2f538062
Bolun Dai <bd1555@nyu.edu>
2024-01-23 23:09:39 -0500

    highlevel interface frequency changed to 200Hz

--- deploy/docker/launch/hw_nodes/orin.launch.py
+++ deploy/docker/launch/hw_nodes/orin.launch.py
@@ -8,26 +8,26 @@ from launch_ros.actions import Node
 def generate_launch_description():
     return LaunchDescription([
         # launch the pointcloud to laser scan converter
-        # Node(
-        #     package='pointcloud_to_laserscan', executable='pointcloud_to_laserscan_node',
-        #     remappings=[('cloud_in', '/b1/rslidar_points'),
-        #                 ('scan', '/b1/rslidar_scans')],
-        #     parameters=[{
-        #         'target_frame': 'cloud',
-        #         'transform_tolerance': 0.01,
-        #         'min_height': 0.0,
-        #         'max_height': 1.0,
-        #         'angle_min': -1.5708,  # -M_PI/2
-        #         'angle_max': 1.5708,  # M_PI/2
-        #         'angle_increment': 0.0087,  # M_PI/360.0
-        #         'scan_time': 0.3333,
-        #         'range_min': 0.45,
-        #         'range_max': 100.0,
-        #         'use_inf': True,
-        #         'inf_epsilon': 1.0
-        #     }],
-        #     name='pointcloud_to_laserscan'
-        # ),
+        Node(
+            package='pointcloud_to_laserscan', executable='pointcloud_to_laserscan_node',
+            remappings=[('cloud_in', '/b1/rslidar_points'),
+                        ('scan', '/b1/rslidar_scans')],
+            parameters=[{
+                'target_frame': 'cloud',
+                'transform_tolerance': 0.01,
+                'min_height': 0.0,
+                'max_height': 1.0,
+                'angle_min': -1.5708,  # -M_PI/2
+                'angle_max': 1.5708,  # M_PI/2
+                'angle_increment': 0.0087,  # M_PI/360.0
+                'scan_time': 0.3333,
+                'range_min': 0.45,
+                'range_max': 100.0,
+                'use_inf': True,
+                'inf_epsilon': 1.0
+            }],
+            name='pointcloud_to_laserscan'
+        ),
 
         # Launch the front looking D455 camera
         IncludeLaunchDescription(
