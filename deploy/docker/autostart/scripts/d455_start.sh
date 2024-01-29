db1359f72867267ae49dc6319e834c1639496e4d
Bolun Dai <bd1555@nyu.edu>
2024-01-29 13:12:42 -0500

    orin docker file modified

--- deploy/docker/autostart/scripts/d455_start.sh
+++ deploy/docker/autostart/scripts/d455_start.sh
@@ -1,3 +1,2 @@
-sleep 2
 WORKSPACE_DIR=/home/robocaster-orin/Documents/B1Py
 source ${WORKSPACE_DIR}/deploy/ros2_ws/install/setup.bash && ros2 launch ${WORKSPACE_DIR}/deploy/docker/launch/hw_nodes/d455.launch.py 
