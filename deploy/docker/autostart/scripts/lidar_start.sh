4df857755255db549c60e133ebc0fa0ea7802424
Rooholla-KhorramBakht <learn.kho@gmail.com>
2024-01-29 12:41:08 -0500

    time delay between auto restarts added

--- deploy/docker/autostart/scripts/lidar_start.sh
+++ deploy/docker/autostart/scripts/lidar_start.sh
@@ -1,2 +1,3 @@
 WORKSPACE_DIR=/home/robocaster-orin/Documents/B1Py
 source ${WORKSPACE_DIR}/deploy/ros2_ws/install/setup.bash && ros2 launch ${WORKSPACE_DIR}/deploy/docker/launch/hw_nodes/lidar.launch.py 
+sleep 2
