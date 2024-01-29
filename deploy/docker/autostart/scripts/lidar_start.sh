e3bc211f0dd791e1a2af21f3082ecf1b007a7c77
Rooholla-KhorramBakht <learn.kho@gmail.com>
2024-01-29 12:44:48 -0500

    startup delay placement changed

--- deploy/docker/autostart/scripts/lidar_start.sh
+++ deploy/docker/autostart/scripts/lidar_start.sh
@@ -1,3 +1,3 @@
+sleep 2
 WORKSPACE_DIR=/home/robocaster-orin/Documents/B1Py
 source ${WORKSPACE_DIR}/deploy/ros2_ws/install/setup.bash && ros2 launch ${WORKSPACE_DIR}/deploy/docker/launch/hw_nodes/lidar.launch.py 
-sleep 2
